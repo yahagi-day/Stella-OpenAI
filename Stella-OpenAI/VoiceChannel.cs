82e245441eb32971fbfcbf41f778961477366a57
yahagi-day <yahagi.day+github@gmail.com>
2023-12-31 18:18:44 +0900

    書き途中

---
+++ Stella-OpenAI/VoiceChannel.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Stella_OpenAI;
+
+public class VoiceChannel
+{
+    public async Task JoinChannel(SocketSlashCommand command)
+    {
+        var channel = (command.User as IGuildUser)?.VoiceChannel;
+        if(channel == null)
+        {
+            await command.RespondAsync("You must be in a voice channel to use this command.");
+            return;
+        }
+        var audioClient = await channel.ConnectAsync();
+    }
+}
